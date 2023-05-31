b41a21a9cff1265dc0d5f2ff08b44d3e5c79a8d5
Karthik-avj <ka.avj99@gmail.com>
2023-05-31 11:15:30 -0700

    read_pomdp working

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,14 +1,15 @@
 using POMDPFiles
 using POMDPModels
 using Test
+using Downloads
 
 pomdp = TigerPOMDP()
 write(stdout, pomdp)
 f1 = tempname()
 f2 = tempname()
-download("http://pomdp.org/examples/1d.POMDP", f1)
+Downloads.download("http://pomdp.org/examples/1d.POMDP", f1)
 m1 = read_pomdp(f1)
-println(m1.discount == 0.75 && m1.O[1,1,1] == 1.0 && m1.T[1,1,1] == 1.0 && m1.R[1,1] == 1.0)
-download("http://pomdp.org/examples/parr95.95.POMDP", f2)
+@test (m1.discount == 0.75 && m1.O[1,1,1] == 1.0 && m1.T[1,1,1] == 1.0 && m1.R[1,1] == 1.0)
+Downloads.download("http://pomdp.org/examples/parr95.95.POMDP", f2)
 m2 = read_pomdp(f2)
-println(m2.discount == 0.95 && m2.O[1,1,1] == 1.0 && m2.T[1,1,2] == 0.5 && m2.R[6,1] == 2.0)
+@test (m2.discount == 0.95 && m2.O[1,1,1] == 1.0 && m2.T[1,1,2] == 0.5 && m2.R[6,1] == 2.0)
