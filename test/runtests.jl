6d1e1f611c4466eaf338dc657badfcd46750459d
John Muchovej <5000729+jmuchovej@users.noreply.github.com>
2023-06-09 19:51:04 -0400

    Refactor tests, add writer tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,18 +4,15 @@ using POMDPTools
 using Test
 using Downloads
 
-pomdp = TigerPOMDP()
-write(stdout, pomdp)
+# Tests drawn from https://pomdp.org/examples/
 
-write(stdout, pomdp; pretty=true)
+const TEST_SOURCES = joinpath(dirname(@__FILE__), "sources")
+const TEST_DATA = joinpath(dirname(@__FILE__), "data")
 
-f1 = tempname()
-f2 = tempname()
-Downloads.download("http://pomdp.org/examples/1d.POMDP", f1)
-m1 = read_pomdp(f1)
-@test POMDPTools.has_consistent_distributions(m1, atol=1e-5)
-@test (m1.discount == 0.75 && m1.O[1,1,1] == 1.0 && m1.T[1,1,1] == 1.0 && m1.R[1,1] == 1.0)
-Downloads.download("http://pomdp.org/examples/parr95.95.POMDP", f2)
-m2 = read_pomdp(f2)
-@test POMDPTools.has_consistent_distributions(m2, atol=1e-5)
-@test (m2.discount == 0.95 && m2.O[1,1,1] == 1.0 && m2.T[2,1,1] == 0.5 && m2.R[6,1] == 2.0)
+println("Running tests:")
+
+println("Running `reader` tests:")
+include("reader.jl")
+
+println("Running `writer` tests:")
+include("writer.jl")
