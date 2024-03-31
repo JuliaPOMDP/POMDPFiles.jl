67d9ede199277627fc33913554c29837949ebca9
licio.romao <licio.romao@gmail.com>
2024-03-31 08:52:30 -0700

    Updating README

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,6 @@
 using HTTP, Gumbo, Cascadia, CodecZlib, Tar
 using POMDPs, POMDPFiles, Test
+using Distributions, StatsBase
 
 include("aux_func.jl")
 
@@ -8,7 +9,6 @@ regex_filename = r"/([^/]*?)\.[Pp][Oo][Mm][Dd][Pp]$"
 ff_pomdp, ff_gz, ff_tar_gz = save_files(false) 
 files_path = [ff_pomdp, ff_gz, ff_tar_gz...]
 all_files_path = read_pomdp_dir(files_path) 
-max_num = [30000,30000] # max number of states and actions of POMDPs to be tested
     
 nn_individual_tests = ["mit", "hallway", "bulkhead.A", "baseball", "tiger.95"]
 individual_tests = set_individual_tests(nn_individual_tests) 
@@ -38,20 +38,29 @@ for file_path in all_files_path
                 end
             end
 
-            if length(actions(pomdp_read)) <= max_num[1] && length(states(pomdp_read)) <= max_num[2] # change this to test all examples
-                if !isempty(pomdp_read.pomdp.support_initialstate)
-                    prob = [val.second for val in initialstate(pomdp_read)]
+            if ff_name in ["baseball"]
+                vec_states = sample(states(pomdp_read), 4, replace=false) 
+                vec_actions = sample(actions(pomdp_read), 4, replace=false)
+            elseif ff_name in ["aloha.30"]
+                vec_states = sample(states(pomdp_read), Int(ceil(0.3*pomdp_read.pomdp.ns)), replace=false) 
+                vec_actions = sample(actions(pomdp_read), Int(ceil(0.3*pomdp_read.pomdp.na)), replace=false) 
+            else
+                vec_states = states(pomdp_read) 
+                vec_actions = actions(pomdp_read)
+            end
+            
+            if !isempty(pomdp_read.pomdp.support_initialstate)
+                prob = [val.second for val in initialstate(pomdp_read)]
+                @test POMDPFiles.test_if_probability(prob)
+            end
+            
+            for s in vec_states
+                for a in vec_actions 
+                    prob = [pomdp_read.pomdp.T[a,s, sp] for sp in states(pomdp_read)]
                     @test POMDPFiles.test_if_probability(prob)
-                end
-                
-                for s in states(pomdp_read) 
-                    for a in actions(pomdp_read)
-                        prob = [pomdp_read.pomdp.T[a,s, sp] for sp in states(pomdp_read)]
-                        @test POMDPFiles.test_if_probability(prob)
-                        
-                        prob = [pomdp_read.pomdp.O[a,s, o] for o in observations(pomdp_read)]
-                        @test POMDPFiles.test_if_probability(prob)  
-                    end
+                    
+                    prob = [pomdp_read.pomdp.O[a,s, o] for o in observations(pomdp_read)]
+                    @test POMDPFiles.test_if_probability(prob)  
                 end
             end
         end
